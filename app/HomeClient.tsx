70c3a12aaa4c20430631f492d6c4d103a6950cf5
bricksonsolana-dot <bricksonsolana@gmail.com>
2026-04-01 13:39:13 +0300

    Hopefully not a lot left

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -296,7 +296,7 @@ export default function HomeClient() {
 
           <div className={styles.companyImageWrap}>
             <ParallaxImage
-              src="/images/home/technology.png"
+              src="/images/home/land.png"
               alt="Kaiser Omnia construction"
             />
           </div>
