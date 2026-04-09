d0f11b491359c18d101a62c7ccbdc8e49dafeceb
Giorgos Spyrou <SFnader@gmail.com>
2026-04-09 07:15:32 +0300

    sanity3

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const securityHeaders = [
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https://cdn.sanity.io",
       "connect-src 'self' https://*.sanity.io https://*.api.sanity.io",
+      "frame-src https://maps.google.com https://www.google.com",
       "frame-ancestors 'none'",
     ].join("; "),
   },
